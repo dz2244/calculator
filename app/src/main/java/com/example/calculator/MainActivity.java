9adba2e6ddd7f5fca227bf7ce1b98a90499eed9d
dz2244 <dz2244@bs.amalnet.k12.il>
2024-03-27 16:18:39 +0200

    הגדרות משתנים

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.calculator;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
-
+    EditText etnd;
+    Button btnResult ,btnCredits ,btnReset ,btnDiv ,btnCefel ,btnMinus ,btnPlus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnResult = findViewById(R.id.btnResult);
+        btnReset = findViewById(R.id.btnReset);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnDiv = findViewById(R.id.btnDiv);
+        btnCefel = findViewById(R.id.btnCefel);
+        btnCredits = findViewById(R.id.btnCredits);
+        etnd = findViewById(R.id.etnd);
+
+
     }
 }
